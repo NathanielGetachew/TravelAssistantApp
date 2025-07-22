1eb9e995ea10a8b2cd79a291b953985459edf9c6
NathanielGetachew <nathigechmaranata@gmail.com>
2025-07-22 12:11:22 +0300

    Fix Jest configuration with updated Babel and tests for Button and fetchTripSuggestions

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,6 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.test.[jt]s?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-native-safe-area-context)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|react-native-safe-area-context|@react-native/js-polyfills)',
   ],
 };
